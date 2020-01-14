3a0c921b9d3101954d0e72a37cba5d9b4aa231fc
Roy Gero <roy.gero.ii@gmail.com>
2020-01-14 15:44:04 -0500

    Updating the Not Found Page.

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -5,7 +5,15 @@ import Header from './Header';
 const NotFoundPage = () => (
   <div>
     <Header/>
-    <div className="content-container">404 - <Link to="/">Go Home</Link></div>
+
+    <div className="content-container">
+      <div className="content-container__title">
+        Page Not Found
+      </div>
+      <div>
+      The page you are trying to access does not exist, please return <Link to="/">home</Link>
+      </div>
+    </div>
   </div>
 );
 
