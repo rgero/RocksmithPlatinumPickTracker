a01d57f72bdf06ddb856e36672f95268bdf3a0e6
Roy Gero <roy.gero.ii@gmail.com>
2020-01-22 15:06:44 -0500

    Adding tests for the Firebase authentication and some simple tests for the song form.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {startLogout} from '../actions/auth';
 
-
-export const Header = () => (
+export const Header = ({ startLogout }) => (
     <header className="header">
         <div className="header__content">
-            <Link className="header__title" to="/" exact="true"><h1>Rocksmith Pick Tracker</h1></Link>
+            <Link className="header__title" to="/practices" exact="true"><h1>Rocksmith Pick Tracker</h1></Link>
+            <button className="button button--link" onClick={startLogout}>Log out</button>
         </div>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+
+    startLogout: () => dispatch(startLogout())
+})
+
+export default connect(undefined, mapDispatchToProps)(Header);
