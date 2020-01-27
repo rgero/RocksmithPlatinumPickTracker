309fbcf265f31a6240febd32bf77afe055de344c
Roy Gero <roy.gero.ii@gmail.com>
2020-01-27 14:02:16 -0500

    Adding Firebase tests.

--- src/tests/actions/songs.test.js
+++ src/tests/actions/songs.test.js
@@ -1,9 +1,22 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import {addSong, setSongs, editSong, removeSong} from '../../actions/songs';
+import {addSong, setSongs, editSong, removeSong, startAddSong, startEditSong, startRemoveSong} from '../../actions/songs';
 import songs from '../fixtures/songs'
+import database from '../../firebase/firebase';
 
+const uid = "testString";
+const defAuthState = {auth: {uid} }
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done)=> {
+    const songsData = {};
+    songs.forEach(({id, date, artist, songName, path, difficulty, level, accuracy, notes})=> {
+        songsData[id] = {date, artist, songName, path, difficulty, level, accuracy, notes};
+    })
+
+    database.ref(`users/${uid}/pick_tracker/songs`).set(songsData).then(()=> done());
+})
+
 
 test('Testing addSong action', ()=> {
     let song = songs[0];
@@ -47,4 +60,75 @@ test('Testing removeSong action', ()=> {
         type: "REMOVE_SONG",
         id: removeID
     });
+})
+
+/* Firebase tests */
+test('should edit song on firebase', (done)=> {
+    var target = songs[1];
+    target.artist = "1331";
+    const store = createMockStore(defAuthState);
+    store.dispatch(startEditSong(target.id, target)).then(()=> {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: "EDIT_SONG",
+            id: target.id,
+            update: target
+
+        });
+        return database.ref(`users/${uid}/pick_tracker/songs/${target.id}`).once('value');
+    }).then((snapshot)=>{
+        expect(snapshot.val()).toEqual(target);
+        done();
+    })
+})
+
+test('should add song to database and store', (done)=> {
+    const store = createMockStore(defAuthState);
+    const songData = {
+        artist: "Rise",
+        songName: "Against",
+        path: "Bass",
+        date: "2021-01-06",
+        level: "Silver",
+        difficulty: "Master",
+        accuracy: "100.00",
+        notes: ""
+    }
+    store.dispatch(startAddSong(songData)).then(()=> {
+        const actions = store.getActions(); // This will return an array of actions
+        // We are going to expect one
+
+        expect(actions[0]).toEqual({
+            type: 'ADD_SONG',
+            song: {
+                id: expect.any(String),
+                ...songData
+            }
+        })
+
+        database.ref(`users/${uid}/pick_tracker/songs/${actions[0].song.id}`).once('value').then(
+            (snapshot)=> {
+                expect(snapshot.val()).toEqual(songData)
+                done();
+            }
+        )
+
+        
+    })
+})
+
+test('should remove song from firebase', (done)=> {
+    var targetSong = songs[0];
+    const store = createMockStore(defAuthState);
+    store.dispatch(startRemoveSong(songs[0].id)).then(()=> {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: "REMOVE_SONG",
+            id: targetSong.id
+        });
+        return database.ref(`users/${uid}/pick_tracker/songs/${targetSong.id}`).once('value');
+    }).then((snapshot)=>{
+        expect(snapshot.val()).toBeFalsy();
+        done();
+    })
 })
--- src/tests/fixtures/songs.js
+++ src/tests/fixtures/songs.js
@@ -1,39 +1,47 @@
 const testSongs = [
     {
+        id: "1",
         artist: "Rise Against",
         songName: "Savior",
         path: "Lead",
         date: "2020-01-06",
         level: "Bronze",
         difficulty: "Master",
-        accuracy: "100.00"
+        accuracy: "100.00",
+        notes: ""
     },
     {
+        id: "2",
         artist: "Rise Against",
         songName: "Savior",
         path: "Bass",
         date: "2020-01-06",
         level: "Platinum",
         difficulty: "Master",
-        accuracy: "100.00"
+        accuracy: "100.00",
+        notes: ""
     },
     {
+        id: "3",
         artist: "Jake's Super Band",
         songName: "RAPTOR!!",
         path: "Lead",
         date: "2020-01-06",
         level: "Silver",
         difficulty: "Easy. To strong",
-        accuracy: "100.00"
+        accuracy: "100.00",
+        notes: ""
     },
     {
+        id: "4",
         artist: "Bob Marley",
         songName: "Some Bob Marley Song",
         path: "Rhythm",
         date: "2020-01-06",
         level: "Gold",
         difficulty: "Master",
-        accuracy: "100.00"
+        accuracy: "100.00",
+        notes: ""
     }
 ]
 
